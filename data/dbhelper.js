6980cbc75fd7b62d55f3dfed226de184587469c0
Sathish G <Sathish.gajendran@gmail.com>
2015-07-07 06:51:39 -0400

    Update dbhelper.js

--- data/dbhelper.js
+++ data/dbhelper.js
@@ -1,6 +1,6 @@
 
 var mongoose=require('mongoose');
 
-mongoose.connect('mongodb://admin:adPass@proximus.modulusmongo.net:27017/dO6hydot');
+mongoose.connect('mongodb://admin:adminPass@proximus.modulusmongo.net:27017/dO6hydot');
 
 module.exports=mongoose;
